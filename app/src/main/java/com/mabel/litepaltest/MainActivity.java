08d8a24f26da2597d130f33a3bc3e5e30a87325d
mabelxue <xueym0106@icloud.com>
2017-05-10 22:15:44 +0800

    update data

--- app/src/main/java/com/mabel/litepaltest/MainActivity.java
+++ app/src/main/java/com/mabel/litepaltest/MainActivity.java
@@ -35,5 +35,29 @@ public class MainActivity extends AppCompatActivity {
                 book.save();
             }
         });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("The Lost Symbol");
+                book.setAuthor("Dan Brown");
+                book.setPages(510);
+                book.setPrice(19.95);
+                book.setPress("Unknown");
+                book.save();
+                // 方式一
+//                book.setPrice(10.99);
+//                book.save();
+                // 方式二
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?", "The Lost Symbol", "Dan Brown");
+                // 恢复指定列的默认值
+//                book.setToDefault("press");
+//                book.updateAll();
+            }
+        });
     }
 }
